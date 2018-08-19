f83ad11b9575f5f34d2c88fe99b32e52989db0a1
Promila Ghosh Monty <promilaghoshmonty@gmail.com>
2018-08-19 23:31:22 +0600

    Create little-dwayne.cpp

---
+++ little-dwayne.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+
+int main()
+{
+    int t,n,i,x,j, ara[100],sum=0;
+    sf ("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        sf ("%d",&x);
+        for(j=0;j<x;j++)
+        {
+            sf ("%d",&ara[j]);
+            sum=sum+ara[j];
+        }
+        pf ("%d\n",sum);
+        sum=0;
+    }
+}
