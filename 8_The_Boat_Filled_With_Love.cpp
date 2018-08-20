cafe429b8604a37f086ac71d5a60c5a32c21a9cd
Promila Ghosh Monty <promilaghoshmonty@gmail.com>
2018-08-20 13:44:44 +0600

    Create 8_The_Boat_Filled_With_Love.cpp

---
+++ 8_The_Boat_Filled_With_Love.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+
+int main()
+{
+   unsigned int N;
+   sf ("%u",&N);
+    pf ("We Love You. We %u Are Here For You.",N);
+
+}
