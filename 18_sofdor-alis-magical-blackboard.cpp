e35232f9e36f005b64c9f92615376f694e2c0718
Promila Ghosh Monty <promilaghoshmonty@gmail.com>
2018-08-21 23:33:57 +0600

    Create 18_sofdor-alis-magical-blackboard.cpp

---
+++ 18_sofdor-alis-magical-blackboard.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf scanf
+#define pf printf
+
+int main()
+{
+   long long int T,i,M,P,N,ara[1000];
+   sf("%d",&T);
+   for(i=0;i<T;i++)
+   {
+        sf("%lld%lld",&M,&P);
+        pf("%lld\n",M*P);
+   }
+}
